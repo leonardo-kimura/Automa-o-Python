a4daf18a0143742accdd9c72cae4342cea1adec8
leonardo-yk <leo.kimura07@gmail.com>
2025-10-01 10:20:48 -0300

    .

--- gerenciamente_arquivos/aula_open/main.py
+++ gerenciamente_arquivos/aula_open/main.py
@@ -11,8 +11,8 @@
 # with open("arquivo.txt", 'a', encoding='utf-8') as arquivo:  w-escrever e vai substituir tudo, a-só adicionar
 #     arquivo.write("Olá mundo!\n")
 
-with open("arquivo.txt", 'r+', encoding='utf-8') as arquivo: # w+ usa para se caso o arquivo nao exista, porém vai substituir tudo
-    arquivo.write("Olá mundo!") #depois que escreve o ponteiro ta no final
-    # arquivo.seek(0) #resetar o ponteiro
-    arquivo.write("Texto que vem depois")
-    print(arquivo.read())
+# with open("arquivo.txt", 'r+', encoding='utf-8') as arquivo: # w+ usa para se caso o arquivo nao exista, porém vai substituir tudo
+#     arquivo.write("Olá mundo!") #depois que escreve o ponteiro ta no final
+#     # arquivo.seek(0) #resetar o ponteiro
+#     arquivo.write("Texto que vem depois")
+#     print(arquivo.read())
