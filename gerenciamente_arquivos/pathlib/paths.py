370461e05bbc3d6e15d5899dd9a974789c2c380c
leonardo-yk <leo.kimura07@gmail.com>
2025-10-01 10:50:34 -0300

    .

---
+++ gerenciamente_arquivos/pathlib/paths.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+# caminho = Path("arquivo.txt") #relativo 
+# caminho_absoluto = caminho.absolute()
+
+# print(caminho_absoluto)
+# print(caminho)
+
+#caminho = Path(__file__).parent / "arquivo.txt" # usar caso tenha aberto a pasta do projeto - pois sempre busca o arquivo na mesma pasta do script
+#caminho = Path("arquivo.txt") # usar se abriu a pasta onde esta o arquivo e o script
+
+# if caminho.exists():
+#     print("Existe!")
+# else:
+#     print("Não existe!")
